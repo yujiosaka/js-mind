6ecd2e6687c66ce3d64a54a8b60927b46a8e4e53
shinout <shinout310@gmail.com>
2016-04-06 00:29:07 +0900

    update lib.js for documentation

--- src/lib.js
+++ src/lib.js
@@ -3,37 +3,47 @@
 var linearAlgebra = require("linear-algebra")();
 var Matrix = linearAlgebra.Matrix;
 
-/*
+/**
  * Derivative of the sigmoid function.
+ * @param {Matrix} z
+ * @return {Matrix} converted matrix with the same nRow, nCol
  */
-exports.sigmoidPrime = function(z) {
+export function sigmoidPrime(z) {
   return z.sigmoid().mul(z.sigmoid().mulEach(-1).plusEach(1));
 };
 
-/*
+/**
  * Return a 10-dimensional unit vector with a 1.0 in the j'th position
  * and zeroes elsewhere.  This is used to convert a digit (0...9)
  * into a corresponding desired output from the neural network.
+ * @param {number} j interger: zero to nine
+ * @return {Matrix} 10x1
  */
-exports.vectorizedResult = function(j) {
+export function vectorizedResult(j) {
   var e = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => { return [0]; });
   e[j] = [1];
   return new Matrix(e);
 };
 
-/*
+/**
  * Randomly drop values from a layer.
+ * @param {Matrix} layer
+ * @param {number} pDropout probability to drop out value for each element
+ * @return {Matrix} converted matrix with the same nRow, nCol
  */
-exports.dropoutLayer = function(layer, pDropout) {
+export function dropoutLayer(layer, pDropout) {
   return layer.eleMap(function(elem) {
     return (Math.random() < pDropout ? 0 : elem);
   });
 };
 
-/*
+/**
  * Return a sample from a normal distribution.
+ * @param {number} mu mean
+ * @param {number} sigma sd (must be greater than 0)
+ * @return {number} sample
  */
-let norm = exports.norm = function(mu, sigma) {
+export function norm(mu, sigma) {
   let a = 1 - Math.random();
   let b = 1 - Math.random();
   let c = Math.sqrt(-2 * Math.log(a));
@@ -44,11 +54,15 @@ let norm = exports.norm = function(mu, sigma) {
   }
 }
 
-/*
- * Return samples from the standard normal distribution.
+/**
+ * Return a matrix, all of whose element are sampled from the standard normal distribution.
  * see http://d.hatena.ne.jp/iroiro123/20111210/1323515616
+ *
+ * @param {number} rows the number of rows
+ * @param {number} cols the number of cols
+ * @return {Matrix} random matrix
  */
-exports.randn = function(rows, cols) {
+export function randn(rows, cols) {
   let result = new Array(rows);
   for (let i = 0; i < rows; i++) {
     result[i] = []
