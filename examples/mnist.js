0ed291d4be361928f2185b3e90b07a394f7587bd
yujiosaka <yuji.i@doboken.net>
2016-08-08 01:41:52 +0900

    refactor example

--- examples/mnist.js
+++ examples/mnist.js
@@ -4,16 +4,22 @@ var Promise = require('bluebird');
 
 var jsmind = require('../dist');
 
+var net = new jsmind.Network([
+  new jsmind.layers.ReLULayer(784, 100),
+  new jsmind.layers.SoftmaxLayer(100, 10)
+]);
+
 Promise.all([
   jsmind.MnistLoader.loadTrainingDataWrapper(),
   jsmind.MnistLoader.loadValidationDataWrapper(),
   jsmind.MnistLoader.loadTestDataWrapper()
 ]).spread(function(trainingData, validationData, testData) {
-  var net = new jsmind.Network([
-    new jsmind.layers.ReLULayer(784, 100),
-    new jsmind.layers.SoftmaxLayer(100, 10)
-  ]);
-  net.SGD(trainingData, 60, 10, 0.03, {
+  net.SGD(
+    trainingData,
+    60, // epochs
+    10, // miniBatchSize
+    0.03 // eta
+  , {
     validationData: validationData,
     testData: testData,
     lmbda: 0.1
